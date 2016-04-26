f35511ab8baa2e956d7a0b4c93db26c34fd9423b
Paolo Laurenti <laurentipaolo@gmail.com>
2016-04-26 14:16:10 +0200

    Complete reverse polish calculator

--- calc.erl
+++ calc.erl
@@ -1,10 +1,24 @@
 -module (calc).
--export ([rpn/1]).
+-export ([rpn/1, rpn_test/0]).
 
 rpn(L) when is_list(L) ->
   [Res] = lists:foldl(fun rpn/2, [], string:tokens(L, " ")),
   Res.
 
+rpn("+", [N1, N2|S]) -> [N1+N2|S];
+rpn("-", [N1, N2|S]) -> [N2-N1|S];
+rpn("*", [N1, N2|S]) -> [N1*N2|S];
+rpn("/", [N1, N2|S]) -> [N2/N1|S];
+rpn("^", [N1, N2|S]) -> [math:pow(N2,N1)|S];
+rpn("ln", [N|S]) -> [math:log(N)|S];
+rpn("log10", [N|S]) -> [math:log10(N)|S];
+%my solutions
+%rpn("sum", [N|[]]) -> [N];
+%rpn("sum", [N1, N2|S]) -> rpn("sum", [N1+N2|S]);
+%rpn("prod", [N|[]]) -> [N]; my solution
+%rpn("prod", [N1, N2|S]) -> rpn("prod", [N1*N2|S]);
+rpn("sum", Stack)   -> [lists:sum(Stack)];
+rpn("prod", Stack)  -> [lists:foldl(fun erlang:'*'/2, 1, Stack)];
 rpn(X, Stack) -> [read(X)|Stack].
 
 read(N) ->
@@ -12,3 +26,23 @@ read(N) ->
     {error, no_float} -> list_to_integer(N);
     {F, _} -> F
   end.
+
+rpn_test() ->
+  5 = rpn("2 3 +"),
+  87 = rpn("90 3 -"),
+  -4 = rpn("10 4 3 + 2 * -"),
+  -2.0 = rpn("10 4 3 + 2 * - 2 /"),
+  ok = try
+    rpn("90 34 12 33 55 66 + * - +")
+  catch
+    error:{badarith,[_|_]} -> ok
+  end,
+  4037 = rpn("90 34 12 33 55 66 + * - + -"),
+  8.0 = rpn("2 3 ^"),
+  true = math:sqrt(2) == rpn("2 0.5 ^"),
+  true = math:log(2.7) == rpn("2.7 ln"),
+  true = math:log10(2.7) == rpn("2.7 log10"),
+  50 = rpn("10 10 10 20 sum"),
+  10.0 = rpn("10 10 10 20 sum 5 /"),
+  1000.0 = rpn("10 10 20 0.5 prod"),
+  ok.
